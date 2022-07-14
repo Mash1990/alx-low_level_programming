fcc863a1ee0911b60b3caa308e0ae1b4fb62be75
Mash1990 <79499108+Mash1990@users.noreply.github.com>
2022-07-14 21:20:39 +0300

    Using a different method

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _main_H_
+#define _main_H_
+
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
